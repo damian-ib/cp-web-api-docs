0c85b0397c18455a22461174edbc3c707fa2aa4a
damian-anslik <damiananslik@outlook.com>
2022-08-25 14:33:01 +0100

    added trailing type order information

--- src/docs/changelog/index.js
+++ src/docs/changelog/index.js
@@ -1,5 +1,20 @@
 const changelog = [
     {
+        title: 'Added new topics and improved formatting',
+        date: '2022-08-25',
+        description: 'Added new websockets, endpoints and workflows documentation and improved formatting of code examples',
+        changes: [
+            'New market depth topic added to websockets documentation',
+            'Improved formatting of code examples',
+            'Added information about trailiing type orders to the orders endpoint',
+        ],
+        tags: [
+            'documentation',
+            'websockets',
+            'endpoints',
+        ]
+    },
+    {
         title: 'New API documentation released',
         date: '2022-08-16',
         description: 'New API documentation released',
@@ -13,19 +28,6 @@ const changelog = [
             'documentation',
         ]
     },
-    {
-        title: 'Added new topics and improved formatting',
-        date: '2022-08-25',
-        description: 'Added new websockets documentation topics and improved formatting of code examples',
-        changes: [
-            'New market depth topic added to websockets documentation',
-            'Improved formatting of code examples',
-        ],
-        tags: [
-            'documentation',
-            'websockets',
-        ]
-    }
 ]
 
 export { changelog }
